cdf2bf60f6dcffa161c8309d358f4b3b6cff14f8
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2023-01-09 11:22:10 +0200

    first commit

--- GroundStyles/ContentView.swift
+++ GroundStyles/ContentView.swift
@@ -7,15 +7,99 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+// MARK: - Toggle Shape
+
+struct CheckmarkToggleStyle: ToggleStyle {
+    private let tint: TintShapeStyle = .tint
+
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.label
+            Spacer()
+            Image(systemName: configuration.isOn ? "checkmark" : "square")
+                .resizable()
+                .symbolVariant(configuration.isOn ? .square.fill : .none)
+                .frame(width: 24, height: 24)
+                .foregroundStyle(tint)
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
         }
-        .padding()
+    }
+}
+
+extension ToggleStyle where Self == CheckmarkToggleStyle {
+    static var checkmark: Self { .init() }
+}
+
+// MARK: - Button Shape
+
+struct AccentButton: ButtonStyle {
+    private let tint: TintShapeStyle = .tint
+
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(spacing: 0) {
+            configuration.label
+
+            Rectangle()
+                .fill(tint)
+                .frame(height: 2)
+        }
+        .fixedSize(horizontal: true, vertical: false)
+    }
+}
+
+extension ButtonStyle where Self == AccentButton {
+    static var accent: Self { .init() }
+}
+
+// MARK: - Content View
+
+struct ContentView: View {
+    @State private var isUpdatesEnabled: Bool = false
+    @State private var isTermsAndConditionsEnabled: Bool = false
+
+    var body: some View {
+        Form {
+            Section {
+                Toggle("Daily updates", isOn: $isUpdatesEnabled)
+
+                Toggle("Terms and Conditions", isOn: $isTermsAndConditionsEnabled)
+                    .toggleStyle(.checkmark)
+            }
+
+            Section {
+                Text("Text: Tint has no effect")
+
+                Button("Plain Button: Tint has no effect") {
+                    // do nothing
+                }
+                .buttonStyle(.plain)
+
+                Button("Bordered Button: Tint colors the background") {
+                    // do nothing
+                }
+                .buttonStyle(.bordered)
+
+                Button("Bordered Prominent Button: Tint colors the background") {
+                    // do nothing
+                }
+                .buttonStyle(.borderedProminent)
+
+                Image(systemName: "star")
+
+                Label("Label: Tint has no effect", systemImage: "sun.max")
+            }
+
+            Section {
+                Button("Custom button") {
+                    // do nothing
+                }
+                .buttonStyle(.accent)
+            }
+        }
+        .tint(.orange)
+        .foregroundStyle(.purple)
     }
 }
 
---
+++ README.md
@@ -0,0 +1,10 @@
+# Experiment of using foregroundStyle and tint style on different elements (iOS)
+
+When creating a custom elements in SwiftUI, it is a good idea to utilize the tint color for them for better customization and reusability.
+
+![Screenschot](docs/screenshot.png)
+
+## Versions of used tools
+
+- Xcode 14.2.0
+- iOS 16.2
