4ab5f96d0308e3d65565473fed1ff18efa49af65
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2023-01-09 10:29:50 +0200

    Initial Commit

---
+++ GroundStyles/GroundStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundStylesApp.swift
+//  GroundStyles
+//
+//  Created by Maris Lagzdins on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GroundStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
